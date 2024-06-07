d7f02b404b5111fe85e431cf4d6b941e666c2c4f
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-07 13:10:19 -0400

    colocando texto, mudando cor e alinhamento

--- nosso_primeiro_projeto/lib/main.dart
+++ nosso_primeiro_projeto/lib/main.dart
@@ -11,54 +11,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red,width: 100, height: 100, ),
-                Container(color: Colors.blue,width: 50,height: 50,)
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.blue,width: 100, height: 100, ),
-                Container(color: Colors.red,width: 50,height: 50,)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(color: Colors.cyan,width: 50, height: 50, ),
-                Container(color: Colors.pinkAccent,width: 50,height: 50,),
-                Container(color: Colors.purple,width: 50,height: 50,)
-              ],
-            ),
-            Container(color: Colors.amber,height: 30, width: 300,
-            child: Text('Diamante amarelo'),
-            )
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
         ),
-      )
-    );
+        home: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.red,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.pinkAccent,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text(
+                  'Diamante rosa',
+                  style: TextStyle(color: Colors.pinkAccent, fontSize: 28),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
