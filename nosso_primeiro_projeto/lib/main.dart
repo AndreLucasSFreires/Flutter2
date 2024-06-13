be16cd7a9713d4e3ae667fe98a1e8f06bbfe8896
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-13 19:19:35 -0400

    Jah colocamos um widget de opacidade, envolvendo o listView.. Clicar em botão para clicar e esconder as tarefas, aprendendo a esconder as tarefas

--- nosso_primeiro_projeto/lib/main.dart
+++ nosso_primeiro_projeto/lib/main.dart
@@ -27,34 +27,38 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(leading: Container(), title: Text('Tarefas')),
-          body: ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              Task(
-                  'Aprender Flutter',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                  3),
-              Task(
-                  'Andar de Bike',
-                  'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                  2),
-              Task(
-                  'Orar',
-                  'https://files.adventistas.org/noticias/pt/2022/01/10d64508-aa34-44b6-8ca0-56e656d2714b.jpg',
-                  5),
-              Task(
-                  'Ler a biblia',
-                  'https://www.miliciadaimaculada.org.br/source/files/c/7011/A_Biblia_Sagrada-249504_1208-800-0-0.jpg',
-                  4),
-              Task(
-                  'Estudar 1',
-                  'https://cdn.aprovadoapp.com.br/blog/midia/artigo4-capa.jpg',
-                  5),
-              Task(
-                  'Ver Youtube',
-                  'https://img-21.ccm2.net/Xzq57YZf4NBgVTpboohRrfgvWg4=/500x/22db3f0595ee4cb29016a4ef46e57dc1/ccm-faq/123rf_bloomua.jpg',
-                  1),
-            ],
+          body: AnimatedOpacity(
+            opacity: 1,
+            duration: Duration(milliseconds: 800),
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                Task(
+                    'Aprender Flutter',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    3),
+                Task(
+                    'Andar de Bike',
+                    'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                    2),
+                Task(
+                    'Orar',
+                    'https://files.adventistas.org/noticias/pt/2022/01/10d64508-aa34-44b6-8ca0-56e656d2714b.jpg',
+                    5),
+                Task(
+                    'Ler a biblia',
+                    'https://www.miliciadaimaculada.org.br/source/files/c/7011/A_Biblia_Sagrada-249504_1208-800-0-0.jpg',
+                    4),
+                Task(
+                    'Estudar 1',
+                    'https://cdn.aprovadoapp.com.br/blog/midia/artigo4-capa.jpg',
+                    5),
+                Task(
+                    'Ver Youtube',
+                    'https://img-21.ccm2.net/Xzq57YZf4NBgVTpboohRrfgvWg4=/500x/22db3f0595ee4cb29016a4ef46e57dc1/ccm-faq/123rf_bloomua.jpg',
+                    1),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(onPressed: () {}),
         ));
