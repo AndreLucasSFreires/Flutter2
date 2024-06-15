dda6a95d1145014880cd14e13fc0fb664ac8ed62
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-14 22:59:40 -0400

    Mudando de Stateful para Stateless

--- nosso_primeiro_projeto/lib/main.dart
+++ nosso_primeiro_projeto/lib/main.dart
@@ -5,16 +5,8 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
