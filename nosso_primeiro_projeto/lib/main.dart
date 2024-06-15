817915aec9ee77e3e3b63ec95d5e9177de5dc35f
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-14 22:53:03 -0400

    Movendo a classe InitiaScreen para um novo arquivo.

--- nosso_primeiro_projeto/lib/main.dart
+++ nosso_primeiro_projeto/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:nosso_primeiro_projeto/task.dart';
+
+import 'initial_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,63 +37,7 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class InitialScreen extends StatefulWidget {
-  const InitialScreen({Key? key}) : super(key: key);
-//não colocamos a opacidade aqui porque não estamos recebendo ela.
-  @override
-  State<InitialScreen> createState() => _InitialScreenState();
-}
 
-class _InitialScreenState extends State<InitialScreen> {
-  bool opacidade = true;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(leading: Container(), title: Text('Tarefas')),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: Duration(milliseconds: 800),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            Task(
-                'Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                3),
-            Task(
-                'Andar de Bike',
-                'https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                2),
-            Task(
-                'Orar',
-                'https://files.adventistas.org/noticias/pt/2022/01/10d64508-aa34-44b6-8ca0-56e656d2714b.jpg',
-                5),
-            Task(
-                'Ler a biblia',
-                'https://www.miliciadaimaculada.org.br/source/files/c/7011/A_Biblia_Sagrada-249504_1208-800-0-0.jpg',
-                4),
-            Task(
-                'Estudar 1',
-                'https://cdn.aprovadoapp.com.br/blog/midia/artigo4-capa.jpg',
-                5),
-            Task(
-                'Ver Youtube',
-                'https://img-21.ccm2.net/Xzq57YZf4NBgVTpboohRrfgvWg4=/500x/22db3f0595ee4cb29016a4ef46e57dc1/ccm-faq/123rf_bloomua.jpg',
-                1),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            opacidade = !opacidade;
-          });
-        },
-        child: Icon(Icons.remove_red_eye),
-      ),
-    );
-  }
-}
 
 
 
