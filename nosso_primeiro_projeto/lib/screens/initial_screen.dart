7dfb2bf1d8e53b0a255869e2c45cfe5ced00e9a9
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-16 16:22:24 -0400

    colocando a navegação para ir, chamar outra tela.

--- nosso_primeiro_projeto/lib/screens/initial_screen.dart
+++ nosso_primeiro_projeto/lib/screens/initial_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
+
 //não colocamos a opacidade aqui porque não estamos recebendo ela.
   @override
   State<InitialScreen> createState() => _InitialScreenState();
@@ -16,30 +18,12 @@ class _InitialScreenState extends State<InitialScreen> {
       body: ListView(
         scrollDirection: Axis.vertical,
         children: const [
-          Task(
-              'Aprender Flutter',
-              'assets/images/dash.png',
-              3),
-          Task(
-              'Andar de Bike',
-              'assets/images/bike.jpeg',
-              2),
-          Task(
-              'Orar',
-              'assets/images/oracao.jpg',
-              5),
-          Task(
-              'Ler a biblia',
-              'assets/images/ler_a_biblia.jpg',
-              4),
-          Task(
-              'Estudar 1',
-              'assets/images/estudar1.jpg',
-              5),
-          Task(
-              'Ver Youtube',
-              'assets/images/verYoutube.webp',
-              1),
+          Task('Aprender Flutter', 'assets/images/dash.png', 3),
+          Task('Andar de Bike', 'assets/images/bike.jpeg', 2),
+          Task('Orar', 'assets/images/oracao.jpg', 5),
+          Task('Ler a biblia', 'assets/images/ler_a_biblia.jpg', 4),
+          Task('Estudar 1', 'assets/images/estudar1.jpg', 5),
+          Task('Ver Youtube', 'assets/images/verYoutube.webp', 1),
           SizedBox(
             height: 80,
           )
@@ -47,9 +31,13 @@ class _InitialScreenState extends State<InitialScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => FormScreen(),
+              ));
         },
-        child: const Icon(Icons.remove_red_eye),
+        child: const Icon(Icons.add),
       ),
     );
   }
