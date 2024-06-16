92e5330ffe2c3b384abfe6fe671d3600a2e5dca1
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-16 16:18:33 -0400

    Removendo a função de opacidade

--- nosso_primeiro_projeto/lib/screens/initial_screen.dart
+++ nosso_primeiro_projeto/lib/screens/initial_screen.dart
@@ -9,52 +9,45 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-  bool opacidade = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(leading: Container(), title: const Text('Tarefas')),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: const Duration(milliseconds: 800),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: const [
-            Task(
-                'Aprender Flutter',
-                'assets/images/dash.png',
-                3),
-            Task(
-                'Andar de Bike',
-                'assets/images/bike.jpeg',
-                2),
-            Task(
-                'Orar',
-                'assets/images/oracao.jpg',
-                5),
-            Task(
-                'Ler a biblia',
-                'assets/images/ler_a_biblia.jpg',
-                4),
-            Task(
-                'Estudar 1',
-                'assets/images/estudar1.jpg',
-                5),
-            Task(
-                'Ver Youtube',
-                'assets/images/verYoutube.webp',
-                1),
-            SizedBox(
-              height: 80,
-            )
-          ],
-        ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: const [
+          Task(
+              'Aprender Flutter',
+              'assets/images/dash.png',
+              3),
+          Task(
+              'Andar de Bike',
+              'assets/images/bike.jpeg',
+              2),
+          Task(
+              'Orar',
+              'assets/images/oracao.jpg',
+              5),
+          Task(
+              'Ler a biblia',
+              'assets/images/ler_a_biblia.jpg',
+              4),
+          Task(
+              'Estudar 1',
+              'assets/images/estudar1.jpg',
+              5),
+          Task(
+              'Ver Youtube',
+              'assets/images/verYoutube.webp',
+              1),
+          SizedBox(
+            height: 80,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            opacidade = !opacidade;
-          });
+
         },
         child: const Icon(Icons.remove_red_eye),
       ),
