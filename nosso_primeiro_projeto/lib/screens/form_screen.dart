cd20bc487afd16e779a3773ba04f50290d4d01f1
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-16 15:39:00 -0400

    Snackbar criado e funcionando com sucesso, ao colocar os dados corretamente na tela

--- nosso_primeiro_projeto/lib/screens/form_screen.dart
+++ nosso_primeiro_projeto/lib/screens/form_screen.dart
@@ -128,6 +128,7 @@ class _FormScreenState extends State<FormScreen> {
                         print(nameController.text);
                         print(int.parse(difficultyController.text));
                         print(imageController.text);
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Salvando nova tarefa')));
                       }
                     },
                     child: Text('Adicionar'),
