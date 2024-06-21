fd618b595617d80513fd5345a2956be0e2fa3acc
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-21 19:08:09 -0400

    Adicionando informacoes no InheritedWidget, e como e lista podemos manipular com facilidade

--- nosso_primeiro_projeto/lib/data/task_inherited.dart
+++ nosso_primeiro_projeto/lib/data/task_inherited.dart
@@ -7,16 +7,24 @@ class TaskInherited extends InheritedWidget {
     required Widget child,
   }) : super(key: key, child: child);
 
-  final List<Task> taskList = [Task(nome, foto, dificuldade)];
+  final List<Task> taskList = [
+    Task('Aprender Flutter', 'assets/images/dash.png', 3),
+    Task('Andar de Bike', 'assets/images/bike.jpeg', 2),
+    Task('Orar', 'assets/images/oracao.jpg', 5),
+    Task('Ler a biblia', 'assets/images/ler_a_biblia.jpg', 4),
+    Task('Estudar 1', 'assets/images/estudar1.jpg', 5),
+    Task('Ver Youtube', 'assets/images/verYoutube.webp', 1),
+  ];
 
   static TaskInherited of(BuildContext context) {
-    final TaskInherited? result = context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+    final TaskInherited? result =
+        context.dependOnInheritedWidgetOfExactType<TaskInherited>();
     assert(result != null, 'No TaskInherited found in context');
     return result!;
   }
 
   @override
   bool updateShouldNotify(TaskInherited old) {
-    return ;
+    return;
   }
 }
