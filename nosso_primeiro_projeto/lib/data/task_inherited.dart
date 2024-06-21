219bd581e6409611e7265b2427863ee66b74e7a3
André Lucas Silva Freires <andre.lucas@ed.company>
2024-06-21 19:06:53 -0400

    Aprendendo a manipular os dados no InheritedWidget

---
+++ nosso_primeiro_projeto/lib/data/task_inherited.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/components/task.dart';
+
+class TaskInherited extends InheritedWidget {
+  TaskInherited({
+    Key? key,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final List<Task> taskList = [Task(nome, foto, dificuldade)];
+
+  static TaskInherited of(BuildContext context) {
+    final TaskInherited? result = context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+    assert(result != null, 'No TaskInherited found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(TaskInherited old) {
+    return ;
+  }
+}
